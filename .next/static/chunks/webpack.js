04a22a3265507bbfc2d3b56908879ae681d7de07
getinet amare <getinetamare@gmail.com>
2023-08-22 17:26:43 +0300

    Add a file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3ee70d4769ec58a2"; }
+/******/ 		__webpack_require__.h = function() { return "05fee98f6604717a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
